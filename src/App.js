61c59480afdc8e89d8cd5e163855c5bae3fdfcbb
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-08-31 18:15:44 +1000

    added Home,Pages for pages and Popular,Veggie for components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 style={{"font-family": "Qwitcher Grypen", "font-size":"5rem", "text-align":"center"}}>Eddie's Recipe App</h1>
     </div>
   );
 }
