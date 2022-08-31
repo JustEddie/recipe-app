7f6e918125b558e2243a476a95d25c402a903f46
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-08-31 18:45:28 +1000

    trying to map popular..

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -13,10 +13,19 @@ function Popular() {
         `https://api.spoonacular.com/recipes/random?apiKey=1a520533dda447eca16309cf5f2f3b6f&number=9`
         );
         const data = await api.json();
-
+        setPopular(data.recipes);
     }    
   return (
-    <div>Popular</div>
+    <div>
+        <h3>Popular Picks</h3>
+        <div>
+            {Popular.map((recipe)=>{
+                return (
+                    <div></div>
+                )
+            })}
+        </div>
+    </div>
   )
 }
 
