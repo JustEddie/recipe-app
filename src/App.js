646ca5ec8b658fd7c97b5abbbaf0c2658fab625b
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-08-31 18:17:51 +1000

    components in Home, Home in Pages, Pages in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Pages from './pages/Pages';
 
 function App() {
   return (
     <div className="App">
       <h1 style={{"font-family": "Qwitcher Grypen", "font-size":"5rem", "text-align":"center"}}>Eddie's Recipe App</h1>
+      <Pages />
     </div>
   );
 }
