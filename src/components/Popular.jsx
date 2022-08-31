5b495a9af2e99aae2128f9f12b093cfbfc5bb603
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-08-31 18:22:25 +1000

    fetch spoonacular api

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Popular() {
+    const [poplular, setPopular] = useState([]);
+
+    useEffect(() => {
+      getPopular();
+    }, [])
+    
+    const getPopular = async () => {
+        const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=1a520533dda447eca16309cf5f2f3b6f&number=9`
+        );
+        const data = await api.json();
+
+    }    
   return (
     <div>Popular</div>
   )
