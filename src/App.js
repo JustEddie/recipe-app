5725e26efed15e25e5cdb1939ca759758aca4a36
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-09-01 20:59:40 +1000

    added Router, BrowserRouter, Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Pages from './pages/Pages';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1 style={{"font-family": "Qwitcher Grypen", "font-size":"5rem", "text-align":"center"}}>Eddie's Recipe App</h1>
       <Pages />
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import Cuisine from "./Cuisine";
 import Home from "./Home";
 
+
 function Pages() {
-    return(
-        <div>
-            <Home />
-        </div>
-    )
+  return (
+    <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/cuisine" element={<Cuisine />}/>
+    </Routes>
+  );
 }
 
 export default Pages;
