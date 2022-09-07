c81eff6c6cd6ce7c4d56d11ec58eaea58e803472
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-09-07 17:33:01 +1000

    creating handleSubmit function in SubscribeForm

--- src/pages/SubscriptionForm.jsx
+++ src/pages/SubscriptionForm.jsx
@@ -1,5 +1,4 @@
-import { Button } from "bootstrap";
-import React from "react";
+import React, { useRef, useState } from "react";
 import {
   FormGroup,
   Label,
@@ -9,14 +8,52 @@ import {
 } from "../components/Template";
 
 function SubscriptionForm() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const inputRef = useRef();
+  const formRef = useRef();
+
+  // const getSearch = async (id) => {
+  //   const api = await fetch(
+  //     `https://api.spoonacular.com/recipes/complexSearch?apiKey=52850cdf3fc941fd89b0ac10e71ba2ad&number=12&query=${id}`
+  //   );
+  //   const data = await api.json();
+  //   setSearch(data.results);
+  // };
+
+  // useEffect(() => {
+  //     getSearch(params.search);
+  //   }, [params.search]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    //post
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: {name},email:{email} }),
+    };
+    fetch("https://reqres.in/api/posts", requestOptions)
+      .then((response) => response.json())
+      .then((data) => this.setState({ postId: data.id }));
+  }
+
   return (
     <div>
-      <FormGroup>
-      <Message>Subscribe to get our popular recipes by email!</Message>
-        <Label >Full Name</Label>
-        <SubscriptionInput />
+      <FormGroup onSubmit={handleSubmit} ref={formRef}>
+        <Message>Subscribe to get our popular recipes by email!</Message>
+        <Label>Full Name</Label>
+        <SubscriptionInput
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          ref={inputRef}
+        />
         <Label>Email</Label>
-        <SubscriptionInput />
+        <SubscriptionInput
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          ref={inputRef}
+        />
         <SubscriptionButton>Subscribe</SubscriptionButton>
       </FormGroup>
     </div>
